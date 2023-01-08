2943bb619e9d371ea338ae68224461025acb6ff0
MusienkoYuriy97 <97musienko@gmail.com>
2023-01-08 23:07:22 +0100

    SpringKafka: init project

---
+++ src/main/java/com/example/testkafkaproducer/TestKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.testkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testkafkaproducer/TestKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
